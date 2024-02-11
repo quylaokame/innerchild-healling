8e946a3e276417bf61edb10fc66ad891decdf090
quylaokame <tien.nguyen@enotion.io>
2024-02-11 11:31:59 +0700

    update resize

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 import { game } from "./GameController.js";
 import { SCREEN_WIDTH, SCREEN_HEIGHT, gameConfig } from "./Config.js";
-const canvasElement = document.getElementById("pixi-canvas");
-const gameContainer = document.getElementById("game-container");
-const mainDiv = document.getElementById("main");
 
 gsap.registerPlugin(PixiPlugin);
-gsap.defaults({ease: "none"});
+gsap.defaults({ ease: "none" });
 
 const app = new PIXI.Application({
-	view: canvasElement,
+	view: document.getElementById("pixi-canvas"),
 	resolution: 2,
 	autoDensity: true,
 	backgroundColor: 0x000000,
@@ -18,32 +15,50 @@ const app = new PIXI.Application({
 });
 
 globalThis.__PIXI_APP__ = app;
-globalThis.app = app;
+// globalThis.app = app;
 app.stage.addChild(game);
 
 app.renderer.plugins.interaction.autoPreventDefault = false;
 app.renderer.view.style.touchAction = "auto";
 
-resizeCanvas();
+const gameContainer = document.getElementById("game-container");
+const mainDiv = document.getElementById("main");
 
-function resizeCanvas() {
-	setTimeout(() => {
-		const { width, height } = mainDiv.getBoundingClientRect();
-		const realWidth = width;
-		const realHeight = height;
-		gameContainer.style.width = `${realWidth}px`;
-		gameContainer.style.height = `${realHeight}px`;
-		app.renderer.resize(realWidth, realHeight);
-
-		const screenRatio = realWidth / realHeight;
-		gameConfig.gameHeight = SCREEN_WIDTH / screenRatio;
-		const scale = realWidth / SCREEN_WIDTH;
+const resizeGame = function () {
+	const { width, height } = mainDiv.getBoundingClientRect();
+	gameContainer.style.width = `${width}px`;
+	gameContainer.style.height = `${height}px`;
+	app.renderer.resize(width, height);
+	game.x = app.screen.width / 2;
+	game.y = app.screen.height / 2;
+	let scale;
+	if (game.isInit) {
+		const { gameWidth, gameHeight } = gameConfig;
+		scale = Math.min(width / gameWidth, height / gameHeight);
 		game.scale.set(scale, scale);
-		game.x = app.screen.width / 2;
-		game.y = app.screen.height / 2;
+	} else {
+		const screenRatio = width / height;
+		gameConfig.gameHeight = SCREEN_WIDTH / screenRatio;
+		scale = width / SCREEN_WIDTH;
+		game.scale.set(scale, scale);
 		game.init(gameConfig.gameWidth, gameConfig.gameHeight);
-	}, 1);
+	}
+	// console.error({ width, height , scale});
+	// const { width: w, height: h } = game;
+	// console.error({ w, h, ...gameConfig });
 }
+
+resizeGame();
+
+let timeoutResize;
+const onResize = function () {
+	if (timeoutResize) clearTimeout(timeoutResize);
+	timeoutResize = setTimeout(() => {
+		timeoutResize = null;
+		resizeGame();
+	}, 100);
+}
+window.addEventListener("resize", onResize);
 
 const messageContainer = document.getElementById("messageContainer");
 const messageDiv = document.getElementById("messageContainer");
@@ -54,5 +69,3 @@ const showMessage = function (isShow, message = "") {
 }
 
 game.on("SHOW_MESSAGE", showMessage);
-
-
