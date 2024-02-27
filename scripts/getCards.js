e47308c7b193dbc94cebe62b0993f1d993e2dd35
quylaokame <tien.nguyen@enotion.io>
2024-02-27 10:56:01 +0700

    update service rest

--- scripts/getCards.js
+++ scripts/getCards.js
@@ -1,46 +1,25 @@
 import { serviceRest } from "./utils/serviceRest.js";
 const btnTryOut = document.getElementById("BtnTryOut");
-const inputData = document.getElementById("InputEmail");
-btnTryOut.onclick = ()=>{
+
+let isSentMail = false;
+btnTryOut.onclick = () => {
     getCards();
-    // onClick();
 }
 
-async function getCards() {
-    // const userEmail = "tiennguyen.momo@gmail.com";
-    try {
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", "/api/getCards", true);
-        xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                console.error(xhr.responseText);
-                if (xhr.responseText) {
-                    // window.location.replace("http://localhost:3000/check-mail");
-                }
-            } 
-        };
-        const requestBody = JSON.stringify({ userEmail: "tiennguyen.momo@gmail.com" });
-        xhr.send(requestBody);
-    } catch (err) {
-        console.log(err.message)
-    }
-}
-
-async function onClick(){
-    try {
-        const res = await fetch('/api/getCards', {
-            method: 'POST',
-            body: JSON.stringify({ userEmail: "tiennguyen.momo@gmail.com" }),
-            headers: { 'Content-Type': 'application/json' }
-        })
-        const data = await res.json()
-        if (res.status === 400 || res.status === 401) {
-            console.error("NO RESPONSE");
+function getCards() {
+    if (isSentMail) return;
+    isSentMail = true;
+    serviceRest.post({
+        url: "/api/getCards",
+        data: { userEmail: "tiennguyen.momo@gmail.com" },
+        callback: () => {
+            isSentMail = false;
+            location.assign("/check-mail");
+        },
+        callbackErr: () => {
+            isSentMail = false;
         }
-        console.error(data);
-    } catch (err) {
-        console.log(err.message)
-    }
+    })
 }
+
 
